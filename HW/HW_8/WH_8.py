cc86df54d56b806cd7c193a4c3d18b1333c5477c
mykhailo <mykhailo.sholudko@gmail.com>
2022-11-29 22:13:15 +0200

    HW_8

---
+++ HW/HW_8/WH_8.py
@@ -0,0 +1,116 @@
+from math import pi, sqrt
+
+
+class circle:
+
+
+   def __init__(self, *args):
+       self.r = args[0]
+
+
+   def get_type(self):
+       return 'circle'
+
+
+   def get_sides(self):
+       return f'r = {self.r}'
+
+
+   def get_perimeter(self):
+       return f'perimeter = {2 * pi * self.r}'
+
+
+   def get_area(self):
+       return f'area = {pi * self.r ** 2}'
+
+# c1 = circle(7)
+# print(c1.get_sides())
+# print(c1.get_perimeter())
+# print(c1.get_area())
+
+class rectangle:
+
+
+   def __init__(self, a, b):
+       self.a = a
+       self.b = b
+
+
+   def get_type(self):
+       return 'rectangle'
+
+
+   def get_sides(self):
+       return f'a = {self.a}, b = {self.b}'
+
+
+   def get_perimeter(self):
+       return (self.a + self.b) * 2
+
+
+   def get_area(self):
+       return self.a * self.b
+
+
+class triangle:
+
+
+   def __init__(self, a, b, c):
+       self.a = a
+       self.b = b
+       self.c = c
+
+
+   def get_type(self):
+       return 'triangle'
+
+
+   def get_sides(self):
+       return f'a = {self.a}, b = {self.b}, c = {self.c}'
+
+
+   def get_perimeter(self):
+       global p
+       p = self.a + self.b + self.c
+       return f'perimeter = {p}'
+
+
+   def get_area(self):
+       pp = p / 2
+       return f'area = {sqrt(pp * (pp - self.a) * (pp - self.b) * (pp - self.c))}'
+
+
+def get_class(raw_input):
+    args = raw_input.split()
+    count = len(args)
+    if count == 1:
+        r = int(raw_input)
+        if r <= 0:
+            print('такого кола не існує')
+        else:
+            return circle(r)
+    elif count == 2:
+        a, b = [int(i) for i in args]
+        if a <= 0 or b <= 0:
+            print('такого чотирикутника не існує')
+        else:
+            return rectangle(a, b)
+    elif count == 3:
+        a, b, c = [int(i) for i in args]
+        if a + b <= c or a + c <= b or b + c <= a or a <= 0 or b <= 0 or c <= 0:
+            print('такого трикутника не існує')
+        else:
+            return triangle(a, b, c)
+    else:
+        return None
+if __name__ == '__main__':
+    raw_input = input()
+
+
+
+    object = get_class(raw_input)
+    if object:
+        print(object.get_type())
+        print(object.get_sides())
+        print(object.get_perimeter())
+        print(object.get_area())
