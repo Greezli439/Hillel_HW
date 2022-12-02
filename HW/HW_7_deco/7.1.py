ceee32419bf212c8d2b2bddc6af6896b9f556a6d
mykhailo <mykhailo.sholudko@gmail.com>
2022-12-02 13:17:14 +0200

    Deco

--- HW/HW_7_deco/7.1.py
+++ HW/HW_7_deco/7.1.py
@@ -12,9 +12,24 @@ def super_trace(func=None, *, file=sys.stdout):
     @functools.wraps(func)
     def inner(*args, **kwargs):
         if ENABLE_TRACE:
-            print(len(args), file=file)
-            return func(*args, **kwargs)
+            print(file=file)
+            print(func.__name__, file=file)
+            if args:
+                print(f'Кількість вхідних позиційних аргументів декоруємої функції = {len(args)}', file=file)
+                print(f'Тип і значення кожного позиційного аргументу декоруємої функції ->', end = ' ', file=file)
+                print(*[(type(i), i) for i in args], file=file)
+            if kwargs:
+                print(f'Кількість вхідних іменованих аргументів декоруємої функції = {len(kwargs)}', file=file)
+                print(f'Тип і значення кожного іменованого аргументу декоруємої функції ->', end=' ', file=file)
+                print(*[(type(i), i) for i in kwargs.values()], file=file)
+            res_func = func(*args, **kwargs)
+            if res_func:
+                print('Кількість аргументів, що повертає функція =', len(res_func), file=file)
+                print(f'Кількість, тип та значення що повертає декорована функція =>', end=' ', file=file)
+                print(*[(type(i), i) for i in res_func], file=file)
+        return func(*args, **kwargs)
     return inner
+
 
 
 @super_trace(file=sys.stderr)
@@ -22,11 +37,39 @@ def test1(a, b, c):
     return [a, b, c], a, str(b), float(c)
 
 
+@super_trace()
+def test2(a, b, *, c, d):
+    return [a, b, c, d], a, str(b), float(c), d
+
+
+@super_trace(file=sys.stderr)
+def test3(*, a, b, c, d):
+    return [a, b, c, d], a, str(b), float(c), d
+
+
+@super_trace()
+def test4(a, b, *, c, d):
+    print([a, b, c, d], a, str(b), float(c), d)
+
+
+@super_trace()
+def test5():
+    return [1, 2, 3]
+
+
 if __name__ == '__main__':
+    # test ENABLE_TRACE = False
+    ENABLE_TRACE = False
+    test1(1, 2, 3)
+    test2(1, 2, c=3, d=4)
+    test3(a=1, b=2, c=3, d=4)
+    test4(1, 2, c=3, d=4)
+    test5()
 
-   ENABLE_TRACE = False
-   test1(1, 2, 3)
-
-
-   ENABLE_TRACE = True
-   test1(1, 2, 3)
+    # test ENABLE_TRACE = True
+    ENABLE_TRACE = True
+    test1(1, 2, 3)
+    test2(1, 2, c=3, d=4)
+    test3(a=1, b=2, c=3, d=4)
+    test4(1, 2, c=3, d=4)
+    test5()
