be6598d1aaee3ac06e98c33ed9e11e8106d44985
mykhailo <mykhailo.sholudko@gmail.com>
2022-12-24 18:41:44 +0200

    class.v2

--- HW/HW_8/HW_8.1.py
+++ HW/HW_8/HW_8.1.py
@@ -1,102 +1,82 @@
 from math import pi, sqrt
 
-class circle:
+class Circle:
+    type = 'Circle'
 
 
-   def __init__(self, args):
-       self.r = args
-       self.get_type = self.__get_type()
-       self.get_sides = self.__get_sides()
-       self.get_perimeter = self.__get_perimeter()
-       self.get_area = self.__get_area()
+    def __init__(self, args):
+        self.r = args
 
 
-   def __get_type(self):
-       return 'circle'
+    def get_type(self):
+        return self.type
 
 
-   def __get_sides(self):
-       return f'r = {self.r}'
+    def get_sides(self):
+        return f'r = {self.r}'
 
 
-   def __get_perimeter(self):
-       return f'perimeter = {2 * pi * self.r}'
+    def get_perimeter(self):
+        return f'perimeter = {2 * pi * self.r}'
 
 
-   def __get_area(self):
-       return f'area = {pi * self.r ** 2}'
+    def get_area(self):
+        return f'area = {pi * self.r ** 2}'
 
 
-   def __str__(self):
-       return 'Done'
+
+class Rectangle:
+    type = 'Rectangle'
 
 
-class rectangle:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
 
-   def __init__(self, a, b):
-       self.a = a
-       self.b = b
-       self.get_type = self.__get_type()
-       self.get_sides = self.__get_sides()
-       self.get_perimeter = self.__get_perimeter()
-       self.get_area = self.__get_area()
+    def get_type(self):
+        return self.type
 
 
-   def __get_type(self):
-       return 'rectangle'
+    def get_sides(self):
+        return f'a = {self.a}, b = {self.b}'
 
 
-   def __get_sides(self):
-       return f'a = {self.a}, b = {self.b}'
+    def get_perimeter(self):
+        return f'perimeter = {(self.a + self.b) * 2}'
 
 
-   def __get_perimeter(self):
-       return f'perimeter = {(self.a + self.b) * 2}'
+    def get_area(self):
+        return f'area = {self.a * self.b}'
 
 
-   def __get_area(self):
-       return f'area = {self.a * self.b}'
+
+class Triangle:
+    type = 'Triangle'
+
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
 
 
-   def __str__(self):
-       return 'Done'
+    def get_type(self):
+        return self.type
 
 
-class triangle:
+    def get_sides(self):
+        return f'a = {self.a}, b = {self.b}, c = {self.c}'
 
 
-   def __init__(self, a, b, c):
-       self.a = a
-       self.b = b
-       self.c = c
-       self.get_type = self.__get_type()
-       self.get_sides = self.__get_sides()
-       self.get_perimeter = self.__get_perimeter()
-       self.get_area = self.__get_area()
+    def get_perimeter(self):
+        global p
+        p = self.a + self.b + self.c
+        return f'perimeter = {p}'
 
 
-   def __get_type(self):
-       return 'triangle'
-
-
-   def __get_sides(self):
-       return f'a = {self.a}, b = {self.b}, c = {self.c}'
-
-
-   def __get_perimeter(self):
-       global p
-       p = self.a + self.b + self.c
-       return f'perimeter = {p}'
-
-
-   def __get_area(self):
-       pp = p / 2
-       return f'area = {sqrt(pp * (pp - self.a) * (pp - self.b) * (pp - self.c))}'
-
-
-   def __str__(self):
-       return 'Done'
+    def get_area(self):
+        pp = p / 2
+        return f'area = {sqrt(pp * (pp - self.a) * (pp - self.b) * (pp - self.c))}'
 
 
 def get_class(raw_input):
@@ -107,19 +87,19 @@ def get_class(raw_input):
         if r <= 0:
             print('такого кола не існує')
         else:
-            return circle(r)
+            return Circle(r)
     elif count == 2:
         a, b = [int(i) for i in args]
         if a <= 0 or b <= 0:
             print('такого чотирикутника не існує')
         else:
-            return rectangle(a, b)
+            return Rectangle(a, b)
     elif count == 3:
         a, b, c = [int(i) for i in args]
         if a + b <= c or a + c <= b or b + c <= a or a <= 0 or b <= 0 or c <= 0:
             print('такого трикутника не існує')
         else:
-            return triangle(a, b, c)
+            return Triangle(a, b, c)
     else:
         return None
 
@@ -128,8 +108,8 @@ if __name__ == '__main__':
     raw_input = input()
     object = get_class(raw_input)
     if object:
-        print(object.get_type)
-        print(object.get_sides)
-        print(object.get_perimeter)
-        print(object.get_area)
-        print(object)
+        print(object.get_type())
+        print(object.get_sides())
+        print(object.get_perimeter())
+        print(object.get_area())
+
